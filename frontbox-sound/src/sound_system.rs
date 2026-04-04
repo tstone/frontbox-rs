48f217440808da385ba5581fa4122516ea56940c
tstone <wastingtape@gmail.com>
2026-04-03 20:38:11 -0600

    Enhanced sound playback

--- frontbox-sound/src/sound_system.rs
+++ frontbox-sound/src/sound_system.rs
@@ -1,20 +1,43 @@
+use std::collections::HashMap;
 use std::path::Path;
 
 use cpal::traits::{DeviceTrait, HostTrait};
 use frontbox::prelude::*;
-use kira::sound::static_sound::StaticSoundData;
+use kira::sound::PlaybackState;
+use kira::sound::static_sound::{StaticSoundData, StaticSoundHandle};
+use kira::track::{TrackBuilder, TrackHandle};
 use kira::{
   AudioManager, AudioManagerSettings,
   backend::cpal::{CpalBackend, CpalBackendSettings, Error},
+  sound::Region,
 };
+use kira::{Decibels, Tween};
 
 pub struct SoundSystem {
   manager: AudioManager,
+  sounds: HashMap<&'static str, StaticSoundData>,
+  music_track: TrackHandle,
+  callout_track: TrackHandle,
+  // callouts can only play one at a time. If multiple are triggered in quick succession, they will be queued up and played one after another
+  callout_queue: Vec<&'static str>,
+  active_callout: Option<StaticSoundHandle>,
+  active_music: Option<StaticSoundHandle>,
 }
 
 impl SoundSystem {
-  pub fn raw(manager: AudioManager) -> Self {
-    Self { manager }
+  pub fn raw(mut manager: AudioManager) -> Self {
+    let music_track = manager.add_sub_track(TrackBuilder::default()).unwrap();
+    let callout_track = manager.add_sub_track(TrackBuilder::default()).unwrap();
+
+    Self {
+      manager,
+      sounds: HashMap::new(),
+      music_track,
+      callout_track,
+      active_callout: None,
+      callout_queue: Vec::new(),
+      active_music: None,
+    }
   }
 
   #[allow(unused)]
@@ -68,15 +91,121 @@ impl SoundSystem {
     Ok(Self::raw(manager))
   }
 
-  /// Play a wave file once all the way through
-  pub fn play_sfx(&mut self, path: impl AsRef<Path>) {
+  pub fn preload(&mut self, name: &'static str, path: impl AsRef<Path>) {
     match StaticSoundData::from_file(path.as_ref()) {
       Ok(sound) => {
-        self.manager.play(sound).ok();
+        self.sounds.insert(name, sound);
       }
-      Err(e) => log::error!("Failed to play sound {}: {:?}", path.as_ref().display(), e),
+      Err(e) => log::error!(
+        "Failed to preload sound {}: {:?}",
+        path.as_ref().display(),
+        e
+      ),
     }
+  }
+
+  /// Play a pre-loaded wave file once all the way through
+  pub fn play_sfx(&mut self, key: &'static str) {
+    if let Some(sound) = self.sounds.get(key) {
+      self.manager.play(sound.clone()).ok();
+    } else {
+      log::error!("Sound with key '{}' not found", key);
+    }
+  }
+
+  /// Play the wave AND duck any music that is currently playing, then restore the music volume after the sound finishes
+  pub fn play_callout(&mut self, key: &'static str) {
+    // TODO: does this need to queue them up if multiple callouts are played in quick succession? Or just stop the previous one and start the new one?
+    if let Some(sound) = self.sounds.get(key) {
+      if self.active_callout.is_some() {
+        self.callout_queue.push(key);
+      } else {
+        Self::duck_track(&mut self.music_track, -10.0);
+        self.play_callout_sound(sound.clone());
+      }
+    } else {
+      log::error!("Sound with key '{}' not found", key);
+    }
+  }
+
+  pub fn play_callout_sound(&mut self, sound: StaticSoundData) {
+    match self.callout_track.play(sound) {
+      Ok(handle) => {
+        self.active_callout = Some(handle);
+      }
+      Err(e) => log::error!("Failed to play callout sound: {:?}", e),
+    }
+  }
+
+  // TODO: looping
+  pub fn play_music(&mut self, path: impl AsRef<Path>) {
+    match StaticSoundData::from_file(path.as_ref()) {
+      Ok(sound) => {
+        self.active_music = self.music_track.play(sound).ok();
+      }
+      Err(e) => log::error!("Failed to play music {}: {:?}", path.as_ref().display(), e),
+    }
+  }
+
+  /// Changes the active music track to the new one, fading out the old one and fading in the new one
+  pub fn crossfade_music(&mut self, path: impl AsRef<Path>) {
+    match StaticSoundData::from_file(path.as_ref()) {
+      Ok(sound) => {
+        let new_music = self.music_track.play(sound).ok();
+        if let Some(mut old_music) = self.active_music.replace(new_music.unwrap()) {
+          old_music.stop(Tween {
+            duration: Duration::from_millis(500), // TODO: make this configurable?
+            ..Default::default()
+          });
+        }
+      }
+      Err(e) => log::error!("Failed to play music {}: {:?}", path.as_ref().display(), e),
+    }
+  }
+
+  fn duck_track(track: &mut TrackHandle, amount_db: f32) {
+    track.set_volume(
+      Decibels(amount_db),
+      Tween {
+        duration: Duration::from_millis(200),
+        ..Default::default()
+      },
+    );
+  }
+
+  fn unduck_track(track: &mut TrackHandle) {
+    track.set_volume(
+      Decibels(0.0),
+      Tween {
+        duration: Duration::from_millis(200),
+        ..Default::default()
+      },
+    );
   }
 }
 
-impl System for SoundSystem {}
+impl System for SoundSystem {
+  fn on_tick(&mut self, _delta: Duration, _ctx: &Context, _systems: &Systems) {
+    let SoundSystem {
+      active_callout,
+      music_track,
+      ..
+    } = self;
+
+    // un-duck music if callout finished
+    if active_callout.is_some() {
+      if active_callout.as_ref().unwrap().state() == PlaybackState::Stopped {
+        // check if there are any queued callouts, and if so play the next one instead of un-ducking music
+        if let Some(next_key) = self.callout_queue.pop() {
+          if let Some(sound) = self.sounds.get(next_key) {
+            self.play_callout_sound(sound.clone());
+            return;
+          }
+        } else {
+          self.active_callout = None;
+          Self::unduck_track(music_track);
+        }
+      }
+    }
+  }
+}
