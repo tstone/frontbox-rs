cc08f965d7447cbe20f65cc1069060f3205ff88a
Titus <wastingtape@gmail.com>
2026-08-01 16:17:13 -0600

    Fix a bug in the trough occupancy counting

--- frontbox/src/provided/trough.rs
+++ frontbox/src/provided/trough.rs
@@ -20,7 +20,7 @@ impl Trough {
     Self {
       expected_occupancy: switch_names.len(),
       switch_names,
-      eject_coil_name,      
+      eject_coil_name,
       // set on launch
       last_occupancy: 0,
     }
@@ -33,17 +33,17 @@ impl Trough {
           "Plunger Touch Time",
           "Duration by which the eject plunger is brought into contact with the ball, before full eject",
           Duration::from_millis(5),
-          Ranges::duration(0, 100),
+          Ranges::duration(0, 50),
         ),
         initial_pwm_power: HardwareValue::fixed(
-          Power::percent(50),
+          Power::percent(40),
         ),
         secondary_pwm_power: HardwareValue::Fixed(Power::ZERO),
         secondary_pwm_length: HardwareValue::Fixed(Duration::ZERO),
         kick_length: HardwareValue::config(
           "Eject Time",
           "Duration that the plunger exert full power onto the ball (kick)",
-          Duration::from_millis(22),
+          Duration::from_millis(15),
           Ranges::duration(5, 75),
         ),
         ..Default::default()
@@ -57,48 +57,37 @@ impl Trough {
       .tag(tags::Trough)
   }
 
-  fn on_trough_switch_closed(&mut self, switch_name: &str, ctx: &Context) {
-    if self
-      .switch_names
-      // only look at the last switch (nearest the exit) for occupancy changes
-      .get(self.expected_occupancy - 1)
-      .map(|s| *s == switch_name)
-      .unwrap_or(false)
-    {
-      self.re_evaluate_occupancy(ctx);
-    }
-  }
-
-  fn on_trough_switch_opened(&mut self, switch_name: &str, ctx: &Context) {
-    if self
-      .switch_names
-      // only look at the last switch (nearest the exit) for occupancy changes
-      .get(self.expected_occupancy - 1)
-      .map(|s| *s == switch_name)
-      .unwrap_or(false)
-    {
+  fn on_switch(&mut self, switch_name: &str, ctx: &Context) {
+    if self.switch_names.contains(&switch_name) {
       self.re_evaluate_occupancy(ctx);
     }
   }
 
   fn re_evaluate_occupancy(&mut self, ctx: &Context) {
     let occupancy = self.get_occupancy(ctx);
-    let occ_count = occupancy.len();
+    let closed_count = occupancy.iter().filter(|b| **b).count();
 
-    if occ_count > self.last_occupancy {
-      log::debug!("Ball entered trough, occupancy: {:?}", occupancy);    
-      
+    log::debug!(
+      "Re-evaluating occupancy: {} vs {} {:?}",
+      closed_count,
+      self.last_occupancy,
+      occupancy
+    );
+
+    if closed_count > self.last_occupancy {
+      log::debug!("Ball entered trough, occupancy: {:?}", occupancy);
+
       let is_full = occupancy.iter().all(|&o| o);
       ctx.emit(BallEnteredTrough::new(occupancy));
-      
+
       if is_full {
         ctx.emit(TroughFull);
       }
-      self.last_occupancy = occ_count;
-    } else if occupancy.len() < self.last_occupancy {
+      self.last_occupancy = closed_count;
+    } else if closed_count < self.last_occupancy {
       log::debug!("Ball exited trough, occupancy: {:?}", occupancy);
       ctx.emit(BallExitedTrough::new(occupancy));
-      self.last_occupancy = occ_count;
+      self.last_occupancy = closed_count;
     }
   }
 
@@ -137,14 +126,15 @@ impl Trough {
 
 impl System for Trough {
   fn on_spawn(&mut self, ctx: &Context) {
-    self.last_occupancy = self.get_occupancy(ctx).len();
+    log::debug!("Trough spawn");
+    self.last_occupancy = self.get_occupancy(ctx).iter().filter(|b| **b).count();
   }
 
   fn on_event(&mut self, event: &dyn Event, ctx: &Context) {
     if let Some(e) = event.downcast_ref::<SwitchClosed>() {
-      self.on_trough_switch_closed(&e.switch.name, ctx);
+      self.on_switch(&e.switch.name, ctx);
     } else if let Some(e) = event.downcast_ref::<SwitchOpened>() {
-      self.on_trough_switch_opened(&e.switch.name, ctx);
+      self.on_switch(&e.switch.name, ctx);
     }
   }
 
