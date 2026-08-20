10a3a91749e320ec264e7d1885b5840b88a3124c
Titus <wastingtape@gmail.com>
2026-08-19 21:21:39 -0600

    IdAtZ is thread safe (can be in a system)

--- frontbox/src/led/led_identifications_ext.rs
+++ frontbox/src/led/led_identifications_ext.rs
@@ -1,6 +1,8 @@
 use crate::prelude::*;
 
-pub trait LedIdentificationsExt: Contextual<LedIdentifications> + Sized + 'static {
+pub trait LedIdentificationsExt:
+  Contextual<LedIdentifications> + Send + Sync + Sized + 'static
+{
   fn at_z(self, z_index: i8) -> IdentificationAtZ {
     IdentificationAtZ {
       other: Box::new(self),
@@ -8,11 +10,15 @@ pub trait LedIdentificationsExt: Contextual<LedIdentifications> + Sized + 'stati
     }
   }
 }
-impl<T: Contextual<LedIdentifications> + Sized + 'static> LedIdentificationsExt for T {}
+
+impl<T: Contextual<LedIdentifications> + Sized + Send + Sync + 'static> LedIdentificationsExt
+  for T
+{
+}
 
 #[derive(Clone)]
 pub struct IdentificationAtZ {
-  other: Box<dyn Contextual<LedIdentifications>>,
+  other: Box<dyn Contextual<LedIdentifications> + Send + Sync>,
   z: i8,
 }
 
